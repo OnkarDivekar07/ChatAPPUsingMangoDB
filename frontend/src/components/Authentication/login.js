3acab1d221299e71d7bac5aaafc01269053c03bc
Onkar Divekar <onkardivekar07@gmail.com>
2023-12-11 16:20:49 +0530

    signup and login frontend Created

---
+++ frontend/src/components/Authentication/login.js
@@ -0,0 +1,73 @@
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useState } from "react";
+
+const Login = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [picLoading, setPicLoading] = useState(false);
+
+  const submitHandler = async () => {
+    if (!email || !password) {
+      alert("Please fill in both email and password.");
+      return;
+    }
+    try {
+      setPicLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setPicLoading(false);
+      setEmail("");
+      setPassword("");
+      alert("Login successful!");
+    } catch (error) {
+      console.error("Login error:", error);
+      alert("Login failed. Please check your credentials.");
+      setPicLoading(false);
+    }
+  };
+
+  return (
+    <VStack spacing="5px">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter Your Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={picLoading}
+      >
+        Login
+      </Button>
+    </VStack>
+  );
+};
+
+export default Login;
